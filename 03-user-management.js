3beacdab512accf53aa27eb1b23603dcd37c3580
Joel Nkiere <nkierejoe@gmail.com>
2025-11-02 12:43:35 +0000

    exercice 3

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,10 +5,47 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
-}
+function whoIsAdmin(utilisateurs) {
+	const taille=utilisateurs.length;
+	let tabNom=[];
+for(let i=0;i<taille;i++){
+	const unUtilisateur=utilisateurs[i];
+	if(unUtilisateur.estAdmin===true){
+		tabNom.push(unUtilisateur.nom);
 
+
+	}
+}
+return tabNom;
+}
+const utilisateurs=[
+	{
+		nom:"joe",
+		age:49,
+		estAdmin:true
+	},
+	{
+		nom:"chis",
+		age:59,
+		estAdmin:false
+	},
+	{
+		nom:"Mike",
+		age:40,
+		estAdmin:true
+	},
+	{
+		nom:"nkiere",
+		age:30,
+		estAdmin:true
+	},
+	{
+		nom:"john",
+		age:20,
+		estAdmin: false
+	}
+]
+console.log(whoIsAdmin(utilisateurs));
 module.exports = {
 	whoIsAdmin,
 };
