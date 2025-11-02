1d49705b9eba00dd3f50f002b74d372b51cef1f2
Joel Nkiere <nkierejoe@gmail.com>
2025-11-02 08:03:40 +0000

    exercice 1

--- 01-create-user.js
+++ 01-create-user.js
@@ -12,9 +12,17 @@ La fonction doit retourner un objet contenant ces trois propriétés :
     - estConnecte (boolean)
 */
 
-function createUser() {
-    
-}
+function createUser(nom,age,estConnecte) {
+    return{
+        nom:nom,
+        age:age,
+        estConnecté:estConnecte
+    };
+
+
+};
+
+console.log(createUser("Joe",50,true));
 
 module.exports = {
     createUser,
