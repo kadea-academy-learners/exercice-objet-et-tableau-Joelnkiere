e9b4b9182107e9c78d5ad168dc7b349a4e22dd29
Joel Nkiere <nkierejoe@gmail.com>
2025-11-02 18:09:01 +0000

    exercice 4

--- 04-show-student-bulletin.js
+++ 04-show-student-bulletin.js
@@ -15,8 +15,70 @@
 //     - Moyenne < 10  : "À revoir"
 // Dans le cas où l'élève n'a pas de notes, la moyenne doit être considérée comme 0 et le commentaire "À revoir".
 function showStudentBulletin(eleves) {
-	
-}
+	const bulletins = []; // tableau des résultats
+
+	for (let i = 0; i < eleves.length; i++) {
+		const unEleve = eleves[i];
+		const sesNotes = unEleve.notes;
+
+		let somme = 0;
+		for (let j = 0; j < sesNotes.length; j++) {
+			somme += sesNotes[j];
+		}
+
+		// calcul de la moyenne
+		let moyenne = 0;
+		if (sesNotes.length > 0) {
+			moyenne = somme / sesNotes.length;
+		}
+
+		// arrondir à 2 décimales et convertir en nombre
+		moyenne = Number(moyenne.toFixed(2));
+
+		// déterminer le commentaire
+		let commentaire = "";
+		if (moyenne >= 16) commentaire = "Excellent";
+		else if (moyenne >= 14) commentaire = "Très Bien";
+		else if (moyenne >= 12) commentaire = "Bien";
+		else if (moyenne >= 10) commentaire = "Passable";
+		else commentaire = "À revoir";
+
+		// ajouter dans le tableau de bulletins
+		bulletins.push({
+			nom: unEleve.nom,
+			moyenne: moyenne,
+			commentaire: commentaire
+		});
+	}
+
+	return bulletins;
+};
+
+
+
+const eleves = [
+	{
+		nom: "Joe",
+		notes: nombre = [2, 3, 5, 19, 8]
+	},
+	{
+		nom: "Bobette",
+		notes: nombre = [1, 13, 9, 14, 8]
+	},
+	{
+		nom: "Jason",
+		notes: nombre = [2, 3, 15, 1, 8]
+	},
+	{
+		nom: "celio",
+		notes: nombre = [2, 3, 5, 14, 8]
+	},
+	{
+		nom: "mark",
+		notes: nombre = [2, 3, 5, 1, 8]
+	},
+]
+console.log(showStudentBulletin(eleves));
 
 module.exports = {
 	showStudentBulletin,
