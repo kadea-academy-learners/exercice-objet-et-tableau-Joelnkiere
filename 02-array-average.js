ad44c0117015230518122808f4811d8a7860efb0
Joel Nkiere <nkierejoe@gmail.com>
2025-11-02 08:13:41 +0000

    exercice 2

--- 02-array-average.js
+++ 02-array-average.js
@@ -4,8 +4,20 @@
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
 function averageNote(notes) {
-	
-}
+	let somme=0, moyenne=0;
+	taille=notes.length;
+	for(let i=0;i<taille;i++){
+		somme+=notes[i];
+
+	}
+	moyenne=somme/taille;
+	if(moyenne>=10){
+		return "Réussi"
+	}
+	return"Échoué"
+};
+const notes=[23,5,45,0,4];
+
 
 module.exports = {
 	averageNote,
